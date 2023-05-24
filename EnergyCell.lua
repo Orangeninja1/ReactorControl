b85659d15963a6aedd78d223f6ecd8098d4d71b1
Orangeninja1 <20148534+Orangeninja1@users.noreply.github.com>
2023-05-24 19:22:17 -0400

    Update EnergyCell.lua
    Updated script due to changes in modpack causing an error when trying to get max energy since a command had changed.

--- EnergyCell.lua
+++ EnergyCell.lua
@@ -2,10 +2,10 @@ print("Orange Power Monitor V1.0")
 print("Starting Power Monitoring")
 local Cell = peripheral.wrap("left")
 print("Current energy is: ",Cell.getEnergy())
-print("Current max energy is: ",Cell.getEnergyCapacity())
+print("Current max energy is: ",Cell.getMaxEnergy())
 
 while true do
-    power = (Cell.getEnergy() / Cell.getEnergyCapacity()) * 100
+    power = (Cell.getEnergy() / Cell.getMaxEnergy()) * 100
     print("Current energy percentage: ",power,"%")
     if power < 85 then
         redstone.setOutput("back",true)
